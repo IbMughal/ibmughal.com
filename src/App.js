baa1346f60e52ff618201f143b9971bca3830cbb
unknown <ibrahimhmughal@gmail.com>
2024-01-17 20:07:41 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import { Navbar, Container } from "react-bootstrap"
+
+export const NavBar = () => {
+    return (
+        <nav class="navbar" style="background-color: #FFFFFF;">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src={/src/assets/"favicon 1.png"} alt = "Ibrahim Hassan-Mughal"/>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                        <a class="nav-link" href="#">WORK</a>
+                        <a class="nav-link" href="#">CONTACT</a>
+                        <a class="nav-link" href="#">ABOUT</a>
+                    </div>
+                    <span className="navbar-text">
+                        <div className="social-icon">
+                            <a class = "nav-link" href="#"> <img src=""/> </a>
+                            <a class = "nav-link" href="#"> <img src=""/> </a>
+                            <a class = "nav-link" href="#"> <img src=""/> </a>
+                        </div>
+                    </span>
+                </div>
+            </div>
+        </nav>
+    )
+}
